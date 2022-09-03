b5a1a51088cf6ca8d068de5b2f4985787d6c5309
leowang3268 <71745829+leowang3268@users.noreply.github.com>
2022-09-03 10:31:34 +0800

    Delete HW1 directory

--- HW1/input/in02_precedence.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func main() {
-	println(3 - 4 * (+5 + -8) - 10 / 7)
-	println(3 - 4 * (+5 + -8) - 10 / 7 > -4 % 3 || !true && !!false)
-	println(3.0 - 4.0 * (+5.0 + -8.0) - 10.0 / 7.0)
-	print(3.0 - 4.0 * (+5.0 + -8.0) - 10.0 / 7.0 > -4.0 || !true && !!false)
-}
